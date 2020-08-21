14f13424ff3f8d7469996d9e44eb7a164c31e428
Arrotech <arrotechdesign@gmail.com>
2020-08-21 16:10:52 +0300

    Design Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="app">
-    <h2>Google Clone</h2>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<Header />
+				<Switch>
+					<Route path='/'>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
